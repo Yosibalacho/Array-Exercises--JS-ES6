f3de46491de0d732f38fd687c1565b838120cd8a
Yosibalacho <yosibala@gmail.com>
2022-06-14 19:53:12 +0300

    array commit

---
+++ main.js
@@ -0,0 +1,73 @@
+//תרגילי מערכים- JS ES6 //
+//1//
+// function orderStringsInArray(stringArray) {
+//   stringArray.sort();
+// }
+// orderStringsInArray(["yosi", "aviel", "koni", "batel"]);
+//2//
+// function numbersMinToMax(numsString) {
+//   numsString.sort((a, b) => a - b);
+// }
+// numbersMinToMax([20, 30, 5, 200]);
+//3//
+// function numbersMaxToMin(numsString2) {
+//   numsString2.sort((a, b) => b - a);
+// }
+// numbersMaxToMin([66, 78, 155, 7]);
+//4//
+// let namesArray = [];
+// let inputValue = document.getElementById("first_input");
+// let pushBtn = document.getElementById("btn");
+// pushBtn.onclick = () => {
+//   if (namesArray.length < 10) {
+//     namesArray.push(inputValue.value);
+//   }
+//   inputValue.value = "";
+//   namesArray.sort();
+//   console.log(namesArray.join("-"));
+// };
+//5//
+// let rndAges = [];
+// function createRandomArrayOf10Ages() {
+//   for (let i = 0; i < 10; i++) {
+//     rndAges.push(Math.floor(Math.random() * 100));
+//   }
+//   rndAges.sort((a, b) => b - a);
+//   console.log(rndAges);
+// }
+// createRandomArrayOf10Ages();
+//6//
+// namesArray.join("-")
+//7//
+let companyArray = [
+  "Microsoft",
+  "Apple",
+  "Wix",
+  "AT&T",
+  "Intel",
+  "Facebook",
+  "Nvidia",
+  "Applied-materials",
+  "Sony",
+  "Google",
+];
+let galleryArray = [
+  " https://www.yo-yoo.co.il/coolpics/images/uploads/b8a18a.jpg ",
+  "http://www.eliadyahalom.co.il/media/source/BAW48.jpg",
+  "https://www.photo-art.co.il/wp-content/uploads/2017/09/BY1A0451.jpg",
+  "https://ynet-images1.yit.co.il/picserver5/wcm_upload/2021/02/10/rJbMlE311WO/3.jpg",
+  "https://www.photo-art.co.il/wp-content/uploads/2017/10/SY201412-122817.jpg",
+  "https://www.home-print.co.il/wp-content/uploads/thumbs/B-shutterstock_87240172-33r8t5nrs85de0wfk3z3m2.jpg",
+  "https://cdn.babamail.co.il/Files/2015/1/19/4f3f63b0-4437-44e4-88c6-7d5a500538ed.jpg",
+  "http://www.eliadyahalom.co.il/media/images/%D7%AA%D7%9E%D7%95%D7%A0%D7%95%D7%AA%20%D7%A9%D7%97%D7%95%D7%A8%20%D7%9C%D7%91%D7%9F.jpg",
+  "https://cdn.babamail.co.il/Files/2015/1/19/eda850dc-e15e-4090-afcf-9035c1ec2410.jpg",
+  "https://www.yo-yoo.co.il/coolpics/images/uploads/b3bd42.jpg",
+  "https://iritartphotos.com/tn_Cat_491531_789.jpg",
+  "https://www.izikbi.com/files/catalog/item/thumb/613296_BlackWhite.jpg",
+];
+let objArray = [];
+function createObjects() {
+    for(){
+        
+    }
+}
